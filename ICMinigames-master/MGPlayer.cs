044ff451466d3c9d81dd78dc203984865cb1b390
SlayrofShadows <blade19582@gmail.com>
2017-01-12 21:41:48 -0500

    Create MGPlayer.cs

---
+++ ICMinigames-master/MGPlayer.cs
@@ -0,0 +1,14 @@
+using System;
+using TShockAPI;
+using Terraria;
+using TerrariaAPI.Server;
+using System.Collections.Generic;
+using OTAPI;
+//Unneeded until later
+namespace ICMinigames
+{
+    public class MGPlayer
+    {
+        
+    }
+}
