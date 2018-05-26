50e2d7f88e4f4b2e6684c4127fe4ed48ddb9c927
SlayrofShadows <blade19582@gmail.com>
2018-05-25 20:29:03 -0400

    Adding NNM to Multipurpose

---
+++ ICMultiPurpose-master/ICMultiPurpose.cs
@@ -0,0 +1,65 @@
+//'Import' necessary packages
+using System;
+using TShockAPI;
+using Terraria;
+using TerrariaApi.Server;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace ICMultiPurpose
+{
+	[ApiVersion(2,1)]
+	
+	public class ICMultiPurpose : TerrariaPlugin
+	{
+		public override Version Version
+		{
+			get { return new Version(1, 0); }
+		}
+
+		public override string Name
+		{
+			get { return "ICMultiPurpose"; }
+		}
+
+		public override string Author
+		{
+			get { return "Plad & Slayr"; }
+		}
+
+		public override string Description
+		{
+			get { return "Multi Purpose plugin with any features wanted"; }
+		}
+		
+        public ICMultiPurpose(Main game)
+			: base(game)
+		{
+            Order = +4;
+		}
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.GameUpdate.Deregister(this, GameUpdate);
+            }
+            base.Dispose(disposing);
+        }
+
+
+        public override void Initialize()
+		{
+            ServerApi.Hooks.GameUpdate.Register(this, GameUpdate);
+		}
+
+        void GameUpdate(EventArgs args)
+        {
+            WorldGen.spawnMeteor = false;
+        }
+	}
+}
+		
+		
+		
+		
--- ICMultiPurpose-master/Properties/AssemblyInfo.cs
+++ ICMultiPurpose-master/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ICMultipurpose")]
+[assembly: AssemblyTitle("ICMultiPurpose")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("IC")]
 [assembly: AssemblyProduct("ICMultiPurpose")]
-[assembly: AssemblyCopyright("IC")]
+[assembly: AssemblyCopyright("IC 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
