f3f38c4eb628bbb6965cd389492da720873b9126
SlayrofShadows <blade19582@gmail.com>
2018-05-26 01:47:38 -0400

    Organizational Changes

--- ICMultiPurpose-master/ICMultiPurpose.cs
+++ ICMultiPurpose-master/ICMultiPurpose.cs
@@ -8,35 +8,35 @@ using Microsoft.Xna.Framework;
 
 namespace ICMultiPurpose
 {
-	[ApiVersion(2,1)]
-	
-	public class ICMultiPurpose : TerrariaPlugin
-	{
-		public override Version Version
-		{
-			get { return new Version(1, 0); }
-		}
+    [ApiVersion(2, 1)]
 
-		public override string Name
-		{
-			get { return "ICMultiPurpose"; }
-		}
+    public class ICMultiPurpose : TerrariaPlugin
+    {
+        public override Version Version
+        {
+            get { return new Version(1, 0); }
+        }
 
-		public override string Author
-		{
-			get { return "Plad & Slayr"; }
-		}
+        public override string Name
+        {
+            get { return "ICMultiPurpose"; }
+        }
 
-		public override string Description
-		{
-			get { return "Multi Purpose plugin with any features wanted"; }
-		}
-		
+        public override string Author
+        {
+            get { return "Plad & Slayr"; }
+        }
+
+        public override string Description
+        {
+            get { return "Multi Purpose plugin with any features wanted"; }
+        }
+
         public ICMultiPurpose(Main game)
-			: base(game)
-		{
+            : base(game)
+        {
             Order = +4;
-		}
+        }
 
         protected override void Dispose(bool disposing)
         {
@@ -49,17 +49,19 @@ namespace ICMultiPurpose
 
 
         public override void Initialize()
-		{
+        {
             ServerApi.Hooks.GameUpdate.Register(this, GameUpdate);
-		}
+        }
 
         void GameUpdate(EventArgs args)
         {
+            #region Prevent Natural Meteors
             WorldGen.spawnMeteor = false;
+            Console.WriteLine("Meteor successfully prevented");
+            #endregion Prevent Natural Meteors
         }
-	}
+    }
 }
-		
-		
-		
-		
+
+
+
