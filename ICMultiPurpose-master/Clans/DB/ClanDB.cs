acea79f3d07ee5b980dae1a439d791df171de465
SlayrofShadows <blade19582@gmail.com>
2018-05-27 02:34:11 -0400

    Clan Framework 1

---
+++ ICMultiPurpose-master/Clans/DB/ClanDB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TShockAPI.DB;
+using TShockAPI;
+using MySql.Data.MySqlClient;
+using Mono.Data.Sqlite;
+using System.Data;
+
+namespace ICMultiPurpose.Clans.DB
+{
+    public class ClanDB
+    {
+        private static IDbConnection connection;
+        private static ClanDB _instance;
+        
+        public static ClanDB Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new ClanDB();
+                return _instance;
+            }
+        }
+    }
+}
